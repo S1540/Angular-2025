d3df98a87be0e9545743aa7b084701dc2c6c764e
Shubham Singh <shubhamkumar2452004@gmail.com>
2025-09-07 22:36:55 +0530

    create counter app

---
+++ counter-App/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'counter-App';
+  count = 0;
+  handleClick(value: string) {
+    if (value == 'minus') {
+      if (this.count > 0) {
+        this.count = this.count - 1;
+      }
+    } else if (value == 'plus') {
+      this.count = this.count + 1;
+    } else {
+      this.count = 0;
+    }
+  }
+}
