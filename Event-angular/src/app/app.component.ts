92a975187792cc36a961bba4561acf4e78b6d498
Shubham Singh <shubhamkumar2452004@gmail.com>
2025-09-08 22:26:23 +0530

    update events

---
+++ Event-angular/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Event-angular';
+  handleEvent(event: any) {
+    // let buttonVal = event.target as HTMLButtonElement;
+    console.log(event.type);
+    // console.log(event);
+  }
+}
